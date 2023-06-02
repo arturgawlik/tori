9b587ced73c8400d65684950b925651fc681139c
arturgawlik <artur.gawlik@icloud.com>
2023-06-02 19:06:55 +0200

    inital tori

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -1,49 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
   standalone: true,
+  selector: 'app-index',
   template: `
-    <div>
-      <a href="https://analogjs.org/" target="_blank">
-        <img alt="Analog Logo" class="logo analog" src="/analog.svg" />
-      </a>
+    <div class="overflow-hidden">
+      <div
+        class="flex flex-col shrink h-screen items-center justify-center m-x-10 w-[600px] mx-auto px-10"
+      >
+        <p class="animate-show-from-left text-8xl self-start">Hi</p>
+        <div class="border-2 border-black rounded-xl w-full"></div>
+        <p class="animate-show-from-right text-5xl mt-1 self-start">
+          my name is Wiktoria
+        </p>
+        <p class="animate-show-from-hidden text-xl mt-2 self-start">
+          and I am business analyst
+        </p>
+      </div>
     </div>
-
-    <h2>Analog</h2>
-
-    <h3>The fullstack meta-framework for Angular!</h3>
-
-    <div class="card">
-      <button type="button" (click)="increment()">Count {{ count }}</button>
-    </div>
-
-    <p class="read-the-docs">
-      For guides on how to customize this project, visit the
-      <a href="https://analogjs.org" target="_blank">Analog documentation</a>
-    </p>
   `,
-  styles: [
-    `
-      .logo {
-        will-change: filter;
-      }
-      .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-      }
-      .logo.angular:hover {
-        filter: drop-shadow(0 0 2em #42b883aa);
-      }
-      .read-the-docs {
-        color: #888;
-      }
-    `,
-  ],
 })
-export default class HomeComponent {
-  count = 0;
-
-  increment() {
-    this.count++;
-  }
-}
+export default class HomeComponent {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,48 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
-  content: ['./index.html', './src/**/*.{html,ts,md}'],
+  content: ["./index.html", "./src/**/*.{html,ts,md}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "show-from-left": "fromLeft 1s ease",
+        "show-from-right": "fromRight 2s ease ",
+        "show-from-hidden": "fromHidden 4s ease",
+      },
+      keyframes: (theme) => ({
+        fromLeft: {
+          "0%": { transform: "translateX(-1000%)", opacity: 0 },
+          "60%": { transform: "translateX(-1000%)", opacity: 0 },
+          "100%": { transform: "translateX(0)", opacity: 1 },
+        },
+        fromRight: {
+          "0%": { transform: "translateX(1000%)", opacity: 0 },
+          "60%": { transform: "translateX(1000%)", opacity: 0 },
+          "100%": { transform: "translateX(0)", opacity: 1 },
+        },
+        fromHidden: {
+          "0%": { opacity: 0 },
+          "60%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      }),
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ matchUtilities, theme }) => {
+      matchUtilities(
+        {
+          "animation-delay": (value) => {
+            return {
+              "animation-delay": value,
+            };
+          },
+        },
+        {
+          values: theme("transitionDelay"),
+        }
+      );
+    }),
+  ],
 };
